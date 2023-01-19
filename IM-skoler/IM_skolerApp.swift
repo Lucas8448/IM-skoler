ed4f08da70b981c22e049f1614a9734ba5b00e87
Lucas Bateson <lucas.bateson@gmail.com>
2023-01-19 08:33:37 +0100

    Initial Commit

---
+++ IM-skoler/IM_skolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IM_skolerApp.swift
+//  IM-skoler
+//
+//  Created by Lucas William Bateson on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct IM_skolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
